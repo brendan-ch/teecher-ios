13159fcde90b3ca1cb46bb4e5f2cc5f24a2f3d5a
Brendan Chen <me@bchen.dev>
2025-10-25 09:24:35 -0700

    Initial Commit

---
+++ CameraTutorDemo/CameraTutorDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraTutorDemoApp.swift
+//  CameraTutorDemo
+//
+//  Created by Brendan Chen on 2025.10.25.
+//
+
+import SwiftUI
+
+@main
+struct CameraTutorDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
