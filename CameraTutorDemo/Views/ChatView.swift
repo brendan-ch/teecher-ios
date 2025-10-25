30beb1baf9602c2829a3fe9513eaea667e935f4b
Brendan Chen <me@bchen.dev>
2025-10-25 11:22:09 -0700

    Add scaffolding to send a message

--- CameraTutorDemo/Views/ChatView.swift
+++ CameraTutorDemo/Views/ChatView.swift
@@ -24,6 +24,12 @@ struct ChatView: View {
         TextField("Ask anything", text: $query)
             .textFieldStyle(.roundedBorder)
             .padding()
+            .submitLabel(.send)
+            .onSubmit(submit)
+    }
+    
+    func submit() {
+        // TODO: Send the query and the image to the server
     }
 }
 
