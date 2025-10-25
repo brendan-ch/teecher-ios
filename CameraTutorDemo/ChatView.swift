2acef6820cdb3c6b830bbd2d3f194bae812a5a11
Brendan Chen <me@bchen.dev>
2025-10-25 10:03:22 -0700

    Add ChatView placeholder

---
+++ CameraTutorDemo/ChatView.swift
@@ -0,0 +1,19 @@
+//
+//  ChatView.swift
+//  CameraTutorDemo
+//
+//  Created by Brendan Chen on 2025.10.25.
+//
+
+import SwiftUI
+
+struct ChatView: View {
+    var body: some View {
+        Text("Chat View")
+    }
+}
+
+#Preview {
+    ChatView()
+}
+
